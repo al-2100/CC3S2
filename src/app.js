8c30fc5edae31c2797c57e48ae26d5378229053e
Jorge <alonsoxbarriga@hotmail.com>
2024-08-31 00:36:32 -0500

    Pasos finales

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(Server running on port ${port});
+    console.log(`Server running on port ${port}`);
 });
 module.exports = app;

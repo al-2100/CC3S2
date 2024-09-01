94aa7bb2372c38c74f28af7a8153bc8e44c2fdf8
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-01 04:02:14 -0500

    Update app.test.js

--- Actividad 2/devops-practice/tests/app.test.js
+++ Actividad 2/devops-practice/tests/app.test.js
@@ -2,15 +2,17 @@ const request = require('supertest');
 const app = require('../src/app');
 describe('GET /', () => {
     let server;
+    let port = 3000; // Usa el puerto que el servidor escuchará en producción o pruebas
+    beforeAll((done) => {
+        // Inicia el servidor en el puerto especificado
+        server = app.listen(port, done);
+    });
 
-    beforeAll(() => {
-        server = app.listen(0)
-    })
+    afterAll((done) => {
+        // Cierra el servidor después de que se completen los tests
+        server.close(done);
+    });
 
-    afterAll(() => {
-        server.close()
-    })
-    
     it('should return Hello, World!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);

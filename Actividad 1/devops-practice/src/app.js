28d3d18a5be843aee8b30ab4f997444683a92a2c
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-01 18:23:56 -0500

    fix

--- Actividad 1/devops-practice/src/app.js
+++ Actividad 1/devops-practice/src/app.js
@@ -3,8 +3,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
 module.exports = app;
+if(require.main === module){
+    const port = process.env.PORT || 3000;
+    app.listen(port,()=> {
+        console.log(`Server running on port ${port}`);
+    })
+}
--- Actividad 1/devops-practice/tests/app.test.js
+++ Actividad 1/devops-practice/tests/app.test.js
@@ -1,6 +1,16 @@
 const request = require('supertest');
 const app = require('../src/app');
 describe('GET /', () => {
+    let server;
+
+    beforeAll(() => {
+        server = app.listen(0);
+    });
+
+    afterAll(() => {
+        server.close();
+    });
+
     it('should return Hello, World!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);

388014f693c9e37e8255f172525f6ab4524fc388
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-04 00:55:57 -0500

    Intercambio de puertos de Grafana y app

--- Actividad 2/devops-practice/src/app.js
+++ Actividad 2/devops-practice/src/app.js
@@ -3,9 +3,11 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
 module.exports = app;
+
 if(require.main === module){
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3001;
     app.listen(port,()=> {
         console.log(`Server running on port ${port}`);
     })

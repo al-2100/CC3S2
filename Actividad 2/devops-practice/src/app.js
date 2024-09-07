1cc46e2291af2100ec9054700d9e45ebf693f12e
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-09-07 00:09:47 -0500

    Actividad 2 - ejercicio 2

--- Actividad 2/devops-practice/src/app.js
+++ Actividad 2/devops-practice/src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 module.exports = app;
 
-if(require.main === module){
+if (require.main === module) {
     const port = process.env.PORT || 3001;
-    app.listen(port,()=> {
+    const debug = process.env.DEBUG === 'true';  // Leer la variable de entorno DEBUG
+
+    if (debug) {
+        console.log('Debugging is enabled');
+    } else {
+        console.log('Running in production mode');
+    }
+
+    app.listen(port, () => {
         console.log(`Server running on port ${port}`);
-    })
+    });
 }
